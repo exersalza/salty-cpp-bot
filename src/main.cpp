15ec6d1f1ab98e0bfa2c2ef51a0126f899c05038
exersalza <squidodle44@gmail.com>
2022-11-03 15:11:18 +0100

    mostly test changes, bot is not runnable at current state

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 #include <iomanip>
 #include <dpp/dpp.h>
 #include <fmt/format.h>
-#include "header/config.h"
-#include "header/cogs.h"
-#include "header/utils.h"
+#include "header/config.hpp"
+#include "header/cogs.hpp"
+#include "header/utils.hpp"
 
 // import extern modules from ./cog/*
 
@@ -16,25 +16,27 @@ using namespace std;
 using namespace cfg;
 
 
-int main(int argc, char** argv) {
-    string path = "../config.txt";
+int main(int argc, char **argv) {
+    string path = "../config.json";
     Config config = Config(path);
-    const string token = config.getToken();
+    const string &token = config.getToken();
 
     dpp::cluster bot(token, dpp::i_default_intents | dpp::i_message_content);
 
     dpp::cache<dpp::message> bot_message_cache;
 
+    economy::Shop shop((string &) "test");
+
 
     bot.on_log(dpp::utility::cout_logger());
 
-    bot.on_ready([&bot, &argc, &argv](const dpp::ready_t& event) {
+    bot.on_ready([&bot, &argc, &argv](const dpp::ready_t &event) {
         if (dpp::run_once<struct register_bot_commands>()) {
             bot.global_command_create(
-            dpp::slashcommand()
-                .set_type(dpp::ctxm_user)
-                .set_name("High Five")
-                .set_application_id(bot.me.id)
+                    dpp::slashcommand()
+                            .set_type(dpp::ctxm_user)
+                            .set_name("High Five")
+                            .set_application_id(bot.me.id)
             );
 
             if (argc == 2 and strcmp(argv[1], "--init-commands") != 0) {
@@ -45,31 +47,31 @@ int main(int argc, char** argv) {
 
     });
 
-    bot.on_message_create([&](const dpp::message_create_t& event) {
-       auto* msg = new dpp::message();
+    bot.on_message_create([&](const dpp::message_create_t &event) {
+        auto *msg = new dpp::message();
 
-       *msg = event.msg;
+        *msg = event.msg;
 
-       bot_message_cache.store(msg);
+        bot_message_cache.store(msg);
 
-       stringstream ss(event.msg.content);
-       string cmd;
-       dpp::snowflake msg_id;
-       ss >> cmd;
+        stringstream ss(event.msg.content);
+        string cmd;
+        dpp::snowflake msg_id;
+        ss >> cmd;
 
-       if (cmd == "!get") {
-           ss >> msg_id;
-           dpp::message* find_msg = bot_message_cache.find(msg_id);
+        if (cmd == "!get") {
+            ss >> msg_id;
+            dpp::message *find_msg = bot_message_cache.find(msg_id);
 
-           if ( find_msg != nullptr ) {
-               bot.message_create(dpp::message(event.msg.channel_id, "The content was: " + find_msg->content));
-           } else {
-               bot.message_create(dpp::message(event.msg.channel_id, "No message was found."));
-           }
-       }
+            if (find_msg != nullptr) {
+                bot.message_create(dpp::message(event.msg.channel_id, "The content was: " + find_msg->content));
+            } else {
+                bot.message_create(dpp::message(event.msg.channel_id, "No message was found."));
+            }
+        }
     });
 
-    bot.on_user_context_menu([&](const dpp::user_context_menu_t& event) {
+    bot.on_user_context_menu([&](const dpp::user_context_menu_t &event) {
         if (event.command.get_command_name() == "High Five") {
             dpp::user user = event.get_user();
             dpp::user author = event.command.get_issuing_user();
@@ -78,11 +80,11 @@ int main(int argc, char** argv) {
         }
     });
 
-    bot.on_interaction_create([&bot](const dpp::interaction_create_t& event) {
+    bot.on_interaction_create([&bot](const dpp::interaction_create_t &event) {
 
     });
 
-    while (true) {
+    /*while (true) {
         try {
             bot.start(false);
             bot.log(dpp::ll_debug, "started");
@@ -92,7 +94,7 @@ int main(int argc, char** argv) {
         }
 
         ::sleep(30);
-    }
+    }*/
 
     return 0;
 }
