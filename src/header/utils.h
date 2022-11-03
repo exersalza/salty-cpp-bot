64b3f884e78f05142dd4b8d6b9fe38d94957aca4
exersalza <squidodle44@gmail.com>
2022-11-03 08:55:25 +0100

    added string split

--- src/header/utils.h
+++ src/header/utils.h
@@ -4,7 +4,12 @@
 #ifndef DCBOT_UTILS_H
 #define DCBOT_UTILS_H
 
+#include <vector>
+#include <string>
+
 namespace u {
+    std::vector<std::string> split(std::string& s, const char& del);
+    std::vector<std::string> split(std::string& s, const std::string& del);
 } // u
 
 #endif // DCBOT_UTILS_H
--- src/source/utils.cpp
+++ src/source/utils.cpp
@@ -3,7 +3,48 @@
 //
 
 #include <iostream>
+#include <sstream>
 
 #include "../header/utils.h"
 
 using namespace std;
+
+vector<string> u::split(std::string &s, const char& del) {
+    /** String split into vector with single char delimiter
+     *
+     * @param s : Give the String to split.
+     * @param del : Set the delimiter where to split.
+     * @retuns vector : vector of strings
+     *
+     * **/
+    vector<string> ret;
+    stringstream ss(s);
+    string item;
+
+    while (getline(ss, item, del))
+        ret.push_back(item);
+
+    return ret;
+}
+
+vector<string> u::split(std::string &s, const std::string& del) {
+    /** String split into vector with multi char delimiter
+     *
+     * @param s : Give the String to split.
+     * @param del : Set the delimiter where to split.
+     * @retuns vector : vector of strings
+     *
+     * **/
+    size_t pos_start = 0, pos_end, delim_len = del.length();
+    string token;
+    vector<string> ret;
+
+    while ((pos_end = s.find(del, pos_start)) != string::npos) {
+        token = s.substr(pos_start, pos_end + pos_start);
+        pos_start = pos_end + delim_len;
+        ret.push_back(token);
+    }
+
+    ret.push_back(s.substr(pos_start));
+    return ret;
+}
