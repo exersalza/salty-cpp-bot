c641cd55434e124e57ecbfc2399a313a757e5c15
exersalza <squidodle44@gmail.com>
2022-11-05 00:11:16 +0100

    f

---
+++ src/source/cogs.cpp
@@ -0,0 +1,10 @@
+//
+// Created by julian on 11/2/22.
+//
+
+#include <dpp/dpp.h>
+#include "../../include/cogs.hpp"
+
+void cog::regis_commands(dpp::cluster &bot) {
+    bot.global_command_create(dpp::slashcommand("ping", "Ping pong and Latency check", bot.me.id));
+}
