5ab0eb7602e5f0e1c853aa1a206acc5f097ecafd
exersalza <squidodle44@gmail.com>
2022-11-04 12:36:32 +0100

    ticket system init

---
+++ src/header/ticket.hpp
@@ -0,0 +1,16 @@
+//
+// Created by julian on 11/4/22.
+//
+
+#ifndef DCBOT_TICKET_HPP
+#define DCBOT_TICKET_HPP
+
+namespace ticket {
+
+    class ticket {
+
+    };
+
+} // ticket
+
+#endif //DCBOT_TICKET_HPP
---
+++ src/source/ticket.cpp
@@ -0,0 +1,6 @@
+//
+// Created by julian on 11/4/22.
+//
+
+#include "../header/ticket.hpp"
+
