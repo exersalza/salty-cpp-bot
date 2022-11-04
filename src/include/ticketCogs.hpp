ab93f7ecbff460129c23ff5724f42bc6254f6a3b
exersalza <squidodle44@gmail.com>
2022-11-04 15:24:26 +0100

    added todo and plan for ticket system

--- src/include/ticketCogs.hpp
+++ src/include/ticketCogs.hpp
@@ -10,6 +10,29 @@
 #include <nlohmann/json.hpp>
 #include <dpp/dpp.h>
 
+/* Todo:
+ *  legend:
+ *      ? -> don't know if this gonna happen
+ *      ! -> necessary for something
+ *      -> -> definition/description
+ *      <> -> optional arguments
+ *      <<>> -> default argument
+ *      [] -> mandatory arguments
+ *      | -> to do combiner
+ *  Global command: ticket
+ *  Sub-commands:
+ *      create -> create init ticket message, enable ticket system
+ *      set -> sets channel group for tickets
+ *      ? change -> change settings
+ *      disable -> disable ticket system for server, gets re-enabled with create
+ *      |
+ *  Command Usage:
+ *   /ticket create <CHANNEL_ID/<current_channel>>
+ *   /ticket set [CATEGORY_ID]
+ *   /ticket disable/enable
+ *
+ * */
+
 namespace ticket {
     class Ticket {
 
