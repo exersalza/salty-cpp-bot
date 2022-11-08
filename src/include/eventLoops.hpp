6e4283760b81c8f9a188c26d14c7199f16e7b683
exersalza <squidodle44@gmail.com>
2022-11-08 15:39:11 +0100

    added eventloops, for later usage

---
+++ src/include/eventLoops.hpp
@@ -0,0 +1,17 @@
+//
+// Created by julian on 11/8/22.
+//
+
+
+#ifndef DCBOT_EVENTLOOPS_HPP
+#define DCBOT_EVENTLOOPS_HPP
+
+#include <fmt/format.h>
+#include <mysql++/mysql++.h>
+#include <dpp/dpp.h>
+
+namespace el {
+    void event_loop();
+}
+
+#endif //DCBOT_EVENTLOOPS_HPP
---
+++ src/source/eventLoops.cpp
@@ -0,0 +1,13 @@
+//
+// Created by julian on 11/8/22.
+//
+
+#include "../include/eventLoops.hpp"
+
+
+void el::event_loop() {
+    while (true) {
+        /*c.ping();*/
+        ::sleep(60 * 60 * 1);
+    }
+}
