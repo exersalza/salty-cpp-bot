afa766462ef540df6da943c9c3b91510fcb335f8
exersalza <squidodle44@gmail.com>
2022-11-22 09:24:36 +0100

    added example config and added new warning tag for real data

--- src/include/config.hpp
+++ src/include/config.hpp
@@ -5,6 +5,9 @@
 #ifndef DCBOT_CONFIG_HPP
 #define DCBOT_CONFIG_HPP
 
+#define SQL_NORMAL_LENGTH 20
+#define SQL_PASSWORD_LENGTH 32
+
 #include <string>
 
 namespace cfg {
@@ -18,11 +21,11 @@ namespace cfg {
         [[nodiscard]] const std::string &getToken() const;
         [[nodiscard]] const std::string &getToken(const std::string& token_type) const;
 
-        // SQL
-        char sql_host[11] = "localhost";
-        char sql_user[6] = "local";
-        char sql_password[1] = "";
-        char sql_db[14] = "salty_cpp_bot";
+        // SQL - Set a to do tag here, when you insert real data. (I think only usable in Clion but not sure.)
+        char sql_host[SQL_NORMAL_LENGTH] = "localhost";
+        char sql_user[SQL_NORMAL_LENGTH] = "local";
+        char sql_password[SQL_PASSWORD_LENGTH] = "";
+        char sql_db[SQL_NORMAL_LENGTH] = "salty_cpp_bot";
 
 
         int b_color = 0xbc3440;
