d29e1721453fc57b77209db510bff8560753149b
exersalza <squidodle44@gmail.com>
2022-11-22 13:23:56 +0100

    added git downloads for missing libs

--- startup.sh
+++ startup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# ARCH SYSTEMS ONLY, USED FOR DOCKERFILE
+#  USED FOR DOCKERFILE, WHEN YOUR SYSTEM CAN RUN .so FILES, YOU CAN ALSO THIS ONE.
+
+echo "Downloading deps..."
+git clone https://github.com/kenexar/salty-cpp-bot-flibs /home/flibs/
+git clone https://github.com/brainboxdotcc/DPP /home/flibs/DPP
 
 # Copy mysql++ src files to te dest folder, reason is that you can't install yay or paru on docker.
-cd /home/flibs/mysql++/
+echo "Copying mysql++ stuff.."
+mkdir /home/flibs/mysql++ || cd /home/flibs/mysql++
 cp ./* /usr/lib/
 # Copy header files.
 cp include/* /usr/include/mysql++/
+
+echo "Setup finished..."
