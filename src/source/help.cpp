0259e9e6a7670fd7de713897f1230a715ee863c4
exersalza <squidodle44@gmail.com>
2022-11-23 15:18:35 +0100

    added help function

--- src/source/help.cpp
+++ src/source/help.cpp
@@ -7,7 +7,7 @@
 void cog::init_help_events(dpp::cluster &bot, mysqlpp::Connection &c, cfg::Config &config) {
     bot.log(dpp::ll_debug, "Initializing 'help_commands'");
 
-    dpp::slashcommand help("help", "help base command", bot.me.id);
+    dpp::slashcommand help("help", "Shows the Help page.", bot.me.id);
 
     bot.global_command_create(help);
 }
@@ -18,6 +18,16 @@ void cog::init_help_commands(dpp::cluster &bot) {
 
 void cog::help_commands(dpp::cluster &bot, const dpp::slashcommand_t &event,
                            const dpp::command_interaction &cmd_data, const cfg::Config &conf) {
-    std::vector<std::string> user_help;
-    std::vector<std::string> admin_help;
+
+    dpp::embed em;
+
+    em.set_title("Help Site")
+      .set_description("For more information press the corresponding button")
+      .set_color(conf.b_color)
+      .set_timestamp(time(nullptr))
+      .set_footer("Kenexar.eu", bot.me.get_avatar_url());
+
+    em.add_field("Ticket", "`/ticket create` to enable your ticket system", true);
+
+    event.reply(dpp::message(event.command.channel_id, em));
 }
