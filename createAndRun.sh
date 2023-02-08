5176ae060865412d2e718063f2cbd46556aa0ad4
exersalza <squidodle44@gmail.com>
2023-02-08 08:42:20 +0100

    changed way how stuff gets displayed.

--- createAndRun.sh
+++ createAndRun.sh
@@ -4,25 +4,27 @@
 # This project is licensed under the GNU 3 license. more information https://github.com/exersalza/salty-cpp-bot/blob/main/LICENSE
 #
 
+set -x
+
 if ! [[ "$1" == "y" ]]
 then
   echo "Please make sure, you have all the config files prepared. When you did run the command with 'y' as argument."
   exit
 fi
 
-echo "Create Container..."
+: === Create Container... ===
 
 docker build -t salty_cpp_bot:$(date "+%y%m%d") .
 sleep 1
 
-echo "Start bot..."
+: === Start bot... ===
 docker run -d salty_cpp_bot:$(date "+%y%m%d")
 
-echo "Start Docker checker..."
+: === Start Docker checker... ===
 # Get the version in a file to survive script/server crashes
-echo "salty_cpp_bot:$(date "+%y%m%d")" > "$HOME"/.salty_cpp_bot_docker_version
+printf "salty_cpp_bot:$(date "+%y%m%d")" > "$HOME"/.salty_cpp_bot_docker_version
 
 screen kill salty_cpp_bot_checker
 screen -dmS salty_cpp_bot_checker ./dockerChecker.py
 
-echo "Successfully started everything, you're good to go. :)"
+: === Successfully started everything, you're good to go. :) ===
--- startup.sh
+++ startup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+set -x
+
 #  USED FOR Dockerfile, WHEN YOUR SYSTEM CAN RUN .so FILES, YOU CAN ALSO THIS ONE.
 
-echo "Downloading deps..."
+: === Downloading deps... ===
 git clone https://github.com/kenexar/salty-cpp-bot-flibs /home/flibs/
 git clone https://github.com/brainboxdotcc/DPP /home/flibs/DPP
 
 # Copy mysql++ src files to te dest folder, reason is that you can't install yay or paru on docker.
-echo "Copying mysql++ stuff.."
+: === Copying mysql++ stuff... ===
 
 if ! [ -d /home/flibs/mysql++ ]; then
   mkdir /home/flibs/mysql++
@@ -15,9 +17,11 @@ fi
 if ! [ -d /usr/include/mysql++/ ]; then
   mkdir /usr/include/mysql++/
 fi
+
+: === Copying files ===
 # Copy files
 cp /home/flibs/mysql++/* /usr/lib/
 cp /home/flibs/mysql++/include/* /usr/include/mysql++/
 
 
-echo "Setup finished..."
+: === Setup finished... ===
